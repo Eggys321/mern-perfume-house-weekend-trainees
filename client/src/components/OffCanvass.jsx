a0129d0f4f54e59552a200db8aae9447e82c828f
eguono imonieroh <imonieroheguono@gmail.com>
2024-10-29 16:32:30 +0100

    fixes:user details on offCanvass

--- client/src/components/OffCanvass.jsx
+++ client/src/components/OffCanvass.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import menuIcon from "../assets/menu.svg";
 import { Link } from "react-router-dom";
+import CartContext from "../context/CartContext";
+import LogOut from "./LogOut";
 
 const OffCanvass = ({ name, ...props }) => {
   const [show, setShow] = useState(false);
+  const {user} = useContext(CartContext)
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const token = localStorage.getItem("perf-token")
+
   return (
     <>
       <div onClick={handleShow}>
@@ -17,7 +22,14 @@ const OffCanvass = ({ name, ...props }) => {
       </div>
       <Offcanvas show={show} onHide={handleClose} {...props}>
         <Offcanvas.Body>
-          <div className="d-flex flex-column container mt-3 gap-3">
+          {token ? <div className="d-flex pt-5 flex-column justify-content-center align-items-center">
+            <h1>   {`Hi, ${user?.firstName} ${user?.lastName}`} </h1>
+            <div className="w-50">
+              <LogOut/>
+            </div>
+           
+
+          </div> : <div className="d-flex flex-column container mt-3 gap-3">
             <button className="login-btn w-100">
               <Link
                 className="text-decoration-none login-link"
@@ -34,7 +46,8 @@ const OffCanvass = ({ name, ...props }) => {
                 Sign Up
               </Link>
             </button>
-          </div>
+          </div> }
+         
         </Offcanvas.Body>
       </Offcanvas>
     </>
